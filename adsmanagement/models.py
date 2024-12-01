3cf7cf6148b48738d709b0e9c2a6433afda5e9fe
mehrnoosh <mehrnooshdadashi@gmail.com>
2024-12-01 19:09:01 +0330

    Add Comment model

--- adsmanagement/models.py
+++ adsmanagement/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Ads(models.Model):
+class Ad(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,3 +10,12 @@ class Ads(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    ad_id = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.content
