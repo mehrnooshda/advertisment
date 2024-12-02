6d5e1554160df79342bb91440a16475d8b22521a
mehrnoosh <mehrnooshdadashi@gmail.com>
2024-12-02 13:06:54 +0330

    modify auth url paths

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import SignUpView, SignInView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('signin/', SignInView.as_view(), name='signin'),
+    path('auth/signup/', SignUpView.as_view(), name='signup'),
+    path('auth/signin/', SignInView.as_view(), name='signin'),
 ]
